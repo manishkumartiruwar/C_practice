1de22d58dea051f863de48acf625e68679aadeca
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2021-05-15 18:11:17 +0530

    Update HelloWorld.c
    1.execution of a   Hello world allow me understand the  basics of the structure of a c program .
    2.importance of main meathod was demonstrated.
    3. basic syntax of a c program was understood

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>   ///  later on i will  explain the code
+
+
+// main is a meathod where  computer starts with main  method and execute all the instructions.
+// bcompile the c program and then execute it. 
+
+
 int main() {
-    printf("Hello, World!");
-    return 0;
-    
-    
-    
-    
-}
+    printf("Hello World");   ///     printf is just a command to print values.
+                            /// ; tell s the program that the instruction is over. 
+                           /// instructions re executed line by line.
+    return 0; 
+    }
 
 
 
