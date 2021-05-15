f5cffa534709e42713b05417f3be748704c7ec90
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2021-05-15 18:23:12 +0530

    Create DrawAShape.c
    1.executing  printf  statements led me to  understand that  instructions are executed sequentially.
    2. use of \n , allowed me to understand the dynamics of newline instruction.
    Sat, 15 May 2021 12:52:26 GMT

---
+++ DrawAShape.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+  printf("||\n");          /// \n executes a new line .
+  printf("||   ||\n");
+  printf("||      ||\n");
+  printf("||          ||\n");
+  printf("||      ||\n");
+  printf("||   ||\n");
+  printf("||   \n");
+  printf("\n");
+  
+  
+  return 0;
+}
+
+
+/* output
+||
+||   ||
+||      ||
+||          ||
+||      ||
+||   ||
+||   
+
+*/
