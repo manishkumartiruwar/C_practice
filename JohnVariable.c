aacec4e8fb597db38079dcb5c5cfee8aa1b87a21
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2021-05-17 14:04:01 +0530

    Update JohnVariable.c
    1. Using variables in the program helped me to understand that their value can be changed, anywhere in the program.
    2. Using   the  instructions  "%s" ," %c" , " %d" , " %f" , "%lf"    led me to understand that  the are used to print   the value of  character string , character ,  number  , float and 
      double integer variables respectively. 
    3. Using    the  instruction    "%o"  , "%x"   led me to understand that   we can print   octal and hexadecimal value of the number.
    08:33:44 UTC   Monday, 17 May 2021

--- JohnVariable.c
+++ JohnVariable.c
@@ -2,30 +2,63 @@
 #include <stdlib.h>
 int main ()
 {
-   char  CharacterName [] = "JohnVariable"  ;  /// char tells the c compiler that a variable of type char is being executed.
+   char  CharacterName [30] = "JohnVariable"  ;  /// char tells the c compiler that a variable of type char is being executed.
                                  /// CharacterName is the  name of the variable being initialised.
                                  /// [] tells the compiler that a bunch of characters are being executed not just one.
   
    int CharacterAge  = 15;      /// int tells the c compiler that a variable of type int is being executed.
                                 //// Characterage is the  name of the variable being initialised.
   
-  printf("CharacterName %s" , &CharacterName);   ///%s for char
-  printf("CharacterAge %d", &CharacterAge);      /// %d for integer
+  printf("There was a man whose name was %s  ." , CharacterName);   ///%s for character String.
+  printf("He was  %d  years old .\n", CharacterAge);      /// %d for integer.
+ 
+   /// no to demonstrate that value of a variable can be changed .
+   
+   
+   
+   CharacterName [30] = "John Wick"  ; /// I  changed the value from  Johnvariable   to     John Wick.
+  
+   CharacterAge  = 7 ;       /// I changed  the   value from 15 to 7 .
+  
+   printf("There was a man whose name was %s  ." , CharacterName);   ///%s for character String.
+  printf("He was  %d  years old .\n", CharacterAge);
 
 
-
-   char ch = 'A';
-   char str[20] = "fresh2refresh.com";
-   float flt = 10.234;
-   int no = 150;
-   double dbl = 20.123456;
-   printf("Character is %c \n", ch);
-   printf("String is %s \n" , str);
-   printf("Float value is %f \n", flt);
-   printf("Integer value is %d\n" , no);
-   printf("Double value is %lf \n", dbl);
-   printf("Octal value is %o \n", no);
-   printf("Hexadecimal value is %x \n", no);
+   char Charactertype = 'A';                        
+   char CharacterString[20] = "abcdefghijklmnop";
+   float Floattype = 10.234;
+   int Number = 150;
+   double DecimalNumber = 20.123456;
+   printf("Character is %c \n", Charactertype);     ///%c   for character.
+   printf("String is %s \n" , CharacterString);     ///%s   for character String.
+   
+   printf("Float value is %f \n", Floattype);      ///%f    for float type variables.
+   printf("Integer value is %d\n" , Number);       ///%d    for basic integer variable.
+   printf("Double value is %lf \n", DecimalNumber);     ///%   for double  integer variable.
+   
+   /// directly printing a number into its octal and decimal function.
+    
+   printf("Octal value is %o \n", Number);           
+   printf("Hexadecimal value is %x \n", Number);
 
   return 0;
 }
+
+
+
+
+
+/* output
+
+
+There was a man whose name was JohnVariable  .He was  15  years old .
+There was a man whose name was JohnVariable  .He was  7  years old .
+Character is A 
+String is abcdefghijklmnop 
+Float value is 10.234000 
+Integer value is 150
+Double value is 20.123456 
+Octal value is 226 
+Hexadecimal value is 96 
+*/
+
